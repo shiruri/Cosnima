b37534dde73f490ddc87c0c38e2e69237df23699
shiruri <jemmeralmoneda58@gmail.com>
2026-04-27 18:14:23 -0700

    Update SecurityConfig.java

--- src/main/java/com/shiro/cosnima/config/SecurityConfig.java
+++ src/main/java/com/shiro/cosnima/config/SecurityConfig.java
@@ -59,10 +59,12 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of(
-                "http://localhost:3000",
-                "http://127.0.0.1:3000"
-        ));
+ config.setAllowedOriginPatterns(List.of(
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        System.getenv("FRONTEND_URL")
+));
+
 
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
